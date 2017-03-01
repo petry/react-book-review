09748d209df066688e9f4852122d27187d091398
Marcos Petry <marcospetry@gmail.com>
2017-03-01 17:00:55 -0300

    create a book list container and rendering on app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+const books = function() {
+  return [
+    { title: 'simply a book' },
+    { title: 'another book' },
+    { title: 'yet another book' },
+    { title: 'awesome book!' },
+    { title: 'Dive into python' },
+  ]
+}
+
+export default books;
